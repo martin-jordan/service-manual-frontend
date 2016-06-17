076b21267495138886f5cc5b55551c673023107d
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2016-06-17 14:43:18 +0000

    Add title, summary and link from point schema example

--- app/presenters/service_manual_service_standard_presenter.rb
+++ app/presenters/service_manual_service_standard_presenter.rb
@@ -1,2 +1,5 @@
 class ServiceManualServiceStandardPresenter < ContentItemPresenter
+  def points
+    content_item["links"]["points"]
+  end
 end
--- test/integration/service_manual_service_standard_test.rb
+++ test/integration/service_manual_service_standard_test.rb
@@ -6,4 +6,26 @@ class ServiceManualServiceStandardTest < ActionDispatch::IntegrationTest
 
     assert page.has_content?("Digital Service Standard"), "No title found"
   end
+
+  test "service standard page has points" do
+    setup_and_visit_example('service_manual_service_standard', 'service_manual_service_standard')
+
+    assert_equal 2, points.length
+
+    within(points[0]) do
+      assert page.has_content?("1. Understand user needs"), "Point not found"
+      assert page.has_content?("Summary goes here"), "Summary not found"
+      assert page.has_link?("Read more about point 1.", href: "/service-manual/service-standard/understand-user-needs"), "Link not found"
+    end
+
+    within(points[1]) do
+      assert page.has_content?("2. Do ongoing user research"), "Point not found"
+      assert page.has_content?("Another summary goes here"), "Summary not found"
+      assert page.has_link?("Read more about point 2.", href: "/service-manual/service-standard/do-ongoing-user-research"), "Link not found"
+    end
+  end
+
+  def points
+    find_all('.point')
+  end
 end
