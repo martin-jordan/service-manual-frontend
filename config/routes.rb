dbbb498d72c10e84efb188327c22df9447f05231
Josh McMillan <josh@joshmcmillan.co.uk>
2016-12-19 16:14:07 +0000

    Redirect /service-manual/search to /search
    At the moment, requests to /service-manual/search return a 404. These
    should really return a 301 round to /search, adding a `filter_manual`
    parameter to scope search to the service manual.
    
    This PR adds a new route to handle that particular situation.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
+
+  get '/service-manual/search', to: redirect { |_, request|
+    query = request.query_parameters.merge(filter_manual: '/service-manual').to_query
+    "/search?#{query}"
+  }
+
   with_options format: false do |r|
     r.get 'healthcheck', to: proc { [200, {}, ['']] }
     r.get '*path' => 'content_items#show', constraints: { path: %r[.*] }
