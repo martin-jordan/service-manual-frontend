40e9e49ed496f80d5805fb84f8c90aed32399034
Oliver Byford <oliver.byford@digital.cabinet-office.gov.uk>
2017-01-06 16:23:24 +0000

    Add Toolkit hero
    This also changes the style of the existing service manual hero to use a slightly bigger font size for the description and adds slightly more whitespace between the title and the description, to be consistent with the toolkit hero.

--- test/integration/service_toolkit_test.rb
+++ test/integration/service_toolkit_test.rb
@@ -13,4 +13,13 @@ class ServiceToolkitTest < ActionDispatch::IntegrationTest
     refute page.has_css?('.improve-this-page'),
       'Improve this page component should not be present on the page'
   end
+
+  test 'the service toolkit displays the introductory hero' do
+    setup_and_visit_example('service_manual_service_toolkit', 'service_manual_service_toolkit')
+
+    assert page.has_content? <<-TEXT
+      Design and build government services
+      All you need to design, build and run services that meet government standards.
+    TEXT
+  end
 end
