047ca728b77ebabfa17e9768160ed785e1446344
Ian James <ian.james@digital.cabinet-office.gov.uk>
2019-09-13 11:41:29 +0100

    Change accordion to GOV.UK Frontend accordion component  - Use accordion component from GOV.UK Frontend via GOV.UK Publishing Components  - Add new helper function to take content and change it to hash shape required by accordion component  - Introduce another helper function to check whether content should be displayed in an accordion  - Tweak existing helper function to see whether accordion section should be expanded  - Add inline documention to accordion presenter methods  - Remove duplicated accordion tests; tests are upstream in GOV.UK Frontend  - Slightly more consistent logic added - an accordion is only used on pages where there are more than two sections.

--- app/presenters/topic_presenter.rb
+++ app/presenters/topic_presenter.rb
@@ -37,10 +37,85 @@ class TopicPresenter < ContentItemPresenter
     @visually_collapsed
   end
 
+  def expanded?
+    !visually_collapsed?
+  end
+
+  def display_as_accordion?
+    groups.count > 1 && visually_collapsed?
+  end
+
+  def accordion_content
+    # Each accordion needs a hash, as shown in the GOV.UK Publishing Components
+    # guide: https://components.publishing.service.gov.uk/component-guide/accordion
+    #
+    # This method returns the content in the required shape from the hash
+    # supplied by the `groups` method.
+    #
+    # The hash for each accordion section is required to have `name`,
+    # `description`, and `linked_items`. Minimal example:
+    # {
+    #   name: 'Accordion section heading',
+    #   description: 'Accordion section summary',
+    #   linked_items: [
+    #     {
+    #       label: 'Link to example',
+    #       href: 'http://example.com',
+    #     }
+    #   ],
+    # }
+
+
+    groups.map { |section|
+      {
+        heading: {
+          text: section.name,
+        },
+        summary: {
+          text: section.description,
+        },
+        content: {
+          html: accordion_section_links(section.linked_items),
+        },
+        expanded: expanded?,
+      }
+    }
+  end
+
 private
 
-  def topic_breadcrumb
-    { title: title }
+  def accordion_section_links links
+    # Expects `links` to be an array of hashes containing `href` and `label`
+    # for the link. For example:
+    #
+    # ```ruby
+    # [
+    #   {
+    #     label: 'Link to example',
+    #     href: 'http://example.com'
+    #   }
+    # ]
+    # ```
+    #
+    # This will return santitised HTML in a string. The above example would
+    # return:
+    #
+    # ```html
+    # <ul class="govuk-list">
+    #   <li>
+    #     <a href="http://example.com" class="govuk-link">Link to example</a>
+    #   </li>
+    # </ul>
+    # ```
+
+    links = links.map { |linked_item|
+      link_html = ActionController::Base.helpers.link_to(linked_item.label, linked_item.href, class: 'govuk-link')
+      "<li>#{link_html}</li>"
+    }
+
+    list = "<ul class=\"govuk-list\">#{links.join('')}</ul>"
+
+    ActionController::Base.helpers.sanitize(list)
   end
 
   def parent_breadcrumbs
@@ -50,5 +125,9 @@ private
         url: '/service-manual'
       }
     ]
+  end
+
+  def topic_breadcrumb
+    { title: title }
   end
 end
