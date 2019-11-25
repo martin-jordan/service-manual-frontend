564c2d782b46702c2087575f50dcc35fdeca893d
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2019-11-25 17:46:35 +0000

    Use CSS sticky for guide menu
    - replaces govuk_frontend_toolkit JS to provide sticky functionality, to keep the left hand menu on a guide visible while scrolling
    - only supported by modern browsers

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,13 +1,5 @@
 //= require govuk_publishing_components/dependencies
 //= require govuk_publishing_components/all_components
 //
-// from govuk_frontend_toolkit and not delivered by static as part of
-// header-footer-only on deployed environments
-//= require govuk/stick-at-top-when-scrolling
-//= require govuk/stop-scrolling-at-footer
-//
 //= require_tree ./govuk
 //= require_tree ./modules
-
-window.GOVUK.stickAtTopWhenScrolling.init();
-window.GOVUK.stopScrollingAtFooter.addEl($('.js-stick-at-top-when-scrolling'));
