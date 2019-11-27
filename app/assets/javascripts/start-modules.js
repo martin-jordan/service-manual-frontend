4a4dcb309356768610ad0f6816627a75d2f37f46
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2019-11-27 15:24:52 +0000

    Remove modules init code
    - handled by the gem now, in dependencies.js, which is already called from application.js

--- app/assets/javascripts/start-modules.js
+++
@@ -1,7 +0,0 @@
-// This is a manifest file only included in test environments
-// from govuk_frontend_toolkit
-//= require govuk/modules
-
-$(document).ready(function () {
-  GOVUK.modules.start();
-});
