98d708365737f8e71c6c9397fc69d528d6e7d106
Issy Long <me@issyl0.co.uk>
2020-03-02 17:55:47 +0000

    Fix Rails/RefuteMethods offenses

--- test/integration/service_toolkit_test.rb
+++ test/integration/service_toolkit_test.rb
@@ -10,7 +10,7 @@ class ServiceToolkitTest < ActionDispatch::IntegrationTest
   test "the service toolkit does not include the new style feedback form" do
     setup_and_visit_example("service_manual_service_toolkit", "service_manual_service_toolkit")
 
-    refute page.has_css?(".improve-this-page"),
+    assert_not page.has_css?(".improve-this-page"),
            "Improve this page component should not be present on the page"
   end
 
