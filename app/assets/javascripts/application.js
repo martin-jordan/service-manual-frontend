d72d05bec2bfbb2484b276dc24e6a1e3a39a4169
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2020-06-05 08:52:17 +0100

    Update individual component includes
    - following a modification of the suggested sass/js functionality, further component files are required by this application because some had been overlooked

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,6 @@
 //= require govuk_publishing_components/lib
 //= require govuk_publishing_components/components/accordion
+//= require govuk_publishing_components/components/button
 //= require govuk_publishing_components/components/feedback
 //= require govuk_publishing_components/components/govspeak
 
